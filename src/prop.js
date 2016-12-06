70dd49078bd78aec53e5ac81d9d29af7983a20bc
Ryan Morr <rm.morr@gmail.com>
2016-12-06 13:06:43 -0500

    Fix typo

--- src/prop.js
+++ src/prop.js
@@ -182,7 +182,7 @@ export default function prop(value) {
      * @return {Number}
      * @api public
      */
-    prop.valueOf = function toString() {
+    prop.valueOf = function valueOf() {
         return this.hashCode();
     };
 
